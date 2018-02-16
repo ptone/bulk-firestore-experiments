dd49be5cf9cdedd3f411eb40cbd3813f1c7067e1
Preston Holmes <ptone@google.com>
2018-02-15 17:10:37 -0800

    working with new client type

--- src/deleteDevices.ts
+++ src/deleteDevices.ts
@@ -1,5 +1,6 @@
 //const google = require('googleapis');
-const google = require("googleapis-async");
+// const google = require("googleapis-async");
+var {google} = require('googleapis');
 const Rx = require('rxjs/Rx');
 
 
@@ -15,12 +16,25 @@ const registryId: string = "other-registry";
 const projectId: string = "iot-provisioning";
 const parentName = `projects/${projectId}/locations/${cloudRegion}`;
 const registryName = `${parentName}/registries/${registryId}`;
+async function getADC() {
+  const res = await google.auth.getApplicationDefault();
+  let client = res.credential;
+  if (client.createScopedRequired && client.createScopedRequired()) {
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+    const scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+    client = client.createScoped(scopes);
+  }
+  return {
+    client: client,
+    projectId: res.projectId
+  }
+}
 
 function deleteDevice(client:any, deviceInfo:any) {
   // let deviceName = {'name':`${registryName}/${deviceInfo.numId}`};
   console.log('deleting', deviceInfo);
   return Rx.Observable.bindNodeCallback(
-    client.projects.locations.registries.devices.delete)({name:`${registryName}/${deviceInfo.numId}`}); 
+    client.projects.locations.registries.devices.delete)({name:`${registryName}/devices/${deviceInfo.numId}`}); 
 }
 
 function listDevices(client:any, pageToken?:string) {
@@ -37,16 +51,27 @@ function listDevices(client:any, pageToken?:string) {
   if (pageToken) {
     request['pageToken'] = pageToken;
   }
+  // client.projects.locations.registries.devices.list(request, function(r, err){
+  //   console.log('result');
+  //   console.log(r);
+  //   console.log('result');
+  //   console.log(err);
+  // });
   return Rx.Observable.bindNodeCallback(
     client.projects.locations.registries.devices.list)(request);
 }
 
 
 async function main() {
-  console.log('hello');
-  let client = await makeClient();
+  // let client = await makeClient();
+  const adc = await getADC();
+  let client = google.cloudiot({
+    version: 'v1',
+    auth: adc.client
+  })
   // console.log(client);
-  console.log('there');
+  // console.log('there');
+  
   let cursor:string;
   
   let items$ = Rx.Observable.of('')
@@ -55,8 +80,12 @@ async function main() {
       .map(r => {
         //console.log('foo');
         //console.log(r[0].nextPageToken);
-        cursor = r[0].nextPageToken;
-        return r[0]
+        //console.log(r.data);
+        cursor = r.data.nextPageToken;
+        return r.data;
+        // console.log('------------------');
+        // cursor = r[0].nextPageToken;
+        // return r[0]
       })
       .flatMap(d => Rx.Observable.from(d.devices))
       }).skip(1);//.take(3000);
